683bbb7943bd155333b7c6f8016b850f2de28760
Alex Carroll <alex.carroll@us.ibm.com>
2017-05-24 14:57:09 -0700

    changing to an express server to proxy requests.

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,46 @@
+'use strict';
+
 var path = require('path');
+var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './app/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index_bundle.js'
-    },
-    module: {
-        rules: [
-            { test: /\.(js)$/, use: 'babel-loader'},
-
-            { test: /\.(scss)$/,
-            use: [{
-                    loader: "style-loader" // creates style nodes from JS strings
-                }, {
-                    loader: "css-loader" // translates CSS into CommonJS
-                }, {
-                    loader: "sass-loader" // compiles Sass to CSS
-                }]
-            }
-        ]
-    },
-    "plugins": [new HtmlWebpackPlugin({
-        template: 'app/index.html'
-    })]
-}
+  devtool: 'eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, 'app/index.js')
+  ],
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html',
+      inject: 'body',
+      filename: 'index.html'
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        "presets": ["react", "es2015", "stage-0", "react-hmre"]
+      }
+    }, {
+      test: /\.json?$/,
+      loader: 'json'
+    }, {
+      test: /\.scss$/, loaders: ['style-loader', 'css-loader', 'sass-loader']
+    }]
+  }
+};
