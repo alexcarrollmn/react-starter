8bc4df4a9f68e51a0dfe197b8ab7c33d119b6717
Alex Carroll <alex.carroll@us.ibm.com>
2017-05-25 11:03:39 -0700

    trying out ba-ui-toolkit

--- app/components/App.js
+++ app/components/App.js
@@ -1,6 +1,8 @@
 var React = require('react');
 var Header = require('./Header/Header');
 var Servers = require('./Servers/Servers');
+import {Button, Checkbox, NumberInput, Radio, RadioGroup, SVGIcon, TextInput, Textarea, ToggleSwitch, Tooltip} from '@ba-ui-toolkit/ba-ui-toolkit';
+
 import './content.scss';
 
 class App extends React.Component {
