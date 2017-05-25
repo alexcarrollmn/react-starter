72835b6c70be1c382d8355faa0bc4161af7fdad2
Alex Carroll <alex.carroll@us.ibm.com>
2017-05-25 08:09:42 -0700

    adding ba-toolkit

--- app/components/App.js
+++ app/components/App.js
@@ -4,18 +4,18 @@ var Servers = require('./Servers/Servers');
 import './content.scss';
 
 class App extends React.Component {
-    render() {
-        return(
-            <div className='container'>
-                <Header />
-                <div className="content--wrapper">
-                    <div className="content">
-                        <Servers />
-                    </div>
-                </div>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className='container'>
+				<Header />
+				<div className="content--wrapper">
+					<div className="content">
+						<Servers />
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
 
 module.exports = App;
