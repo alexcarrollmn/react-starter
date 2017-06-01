7c5b979616aab2567d46173acafd8d7f1c8a2503
Alex Carroll <alex.carroll@us.ibm.com>
2017-05-31 23:14:37 -0700

    search filter is now working

--- app/components/Servers/Servers.js
+++ app/components/Servers/Servers.js
@@ -2,14 +2,15 @@ import React, {Component} from 'react';
 import './servers.scss';
 import {fetchAllServers} from '../../utils/api';
 import {fetchServer} from '../../utils/api';
+import {Button, Checkbox, NumberInput, Radio, RadioGroup, SVGIcon, TextInput, Textarea, ToggleSwitch, Tooltip, Essentials} from '@ba-ui-toolkit/ba-ui-toolkit';
 
 class SelectedStatus extends Component{
 	constructor(props){
 		super();
 	}
+	
 	render(){
 		var statuses = ['all', 'active', 'idle', 'failed'];
-		console.log("this", this.props.servers.status.servers.idle.length);
 		return (
 			<ul className="servers--status">
 				{statuses.map(function(status){
@@ -21,9 +22,10 @@ class SelectedStatus extends Component{
 						failedClass = " servers--status_link-failed"
 					}
 					return(
-						<li className={"servers--status_link" + activeClass + failedClass}
-							onClick={this.props.onSelect.bind(null, status)}
-							key={status}>
+						<li 
+							className={"servers--status_link" + activeClass + failedClass}
+							onClick={() => {this.props.onSelect(status)}}
+							key={status} >
 							{status=='active' ? this.props.servers.status.servers.active.length + ' ': ''}
 							{status=='idle' ? this.props.servers.status.servers.idle.length + ' ': ''} 
 							{status=='failed' ? this.props.servers.status.servers.failed.length + ' ': ''}
@@ -38,24 +40,47 @@ class SelectedStatus extends Component{
 	}
 	
 }
-
+class Search extends Component {
+	constructor(props){
+		super();
+		this.state = {value: ''};
+	}
+	render(){
+		return(
+			<TextInput 
+				className="servers--search"
+				placeholder="search"
+				defaultValue={this.state.value}
+				onChange={val => 
+					{
+						this.props.onSearch(val)
+					}
+				}
+			/>
+		)
+	}
+}
 class ServersTable extends Component{
 	constructor(props){
 		super();
 	}
 	render(){
 		var rows=[];
-
-		this.props.servers.servers.map(function(server, index){
-			console.log(this)
+		
+		let servers = this.props.servers.servers;
+		//filter by search term
+		servers = servers.filter(function(item){
+			return item.name.toLowerCase().search(this.props.search.toLowerCase()) !== -1;
+		}.bind(this))
+		
+		//add servers to table
+		servers.map(function(server, index){
 			if(this.props.status=='all'){
 				rows.push(<ServerRow server={server} key={server.name} />)
 			}
 			else if(this.props.status == server.info.server_status.state){
 				rows.push(<ServerRow server={server} key={server.name} />)
 			}
-			console.log(this.state)
-			console.log("server", server);
 		}.bind(this))
 		return(
 			<table className="servers">
@@ -83,7 +108,6 @@ class ServerRow extends Component{
 		super();
 	}
 	render(){
-		console.log("server row", this);
 		let mem_use = Math.floor((this.props.server.info.server_status.max_mem_use.instant / this.props.server.info.server_status.configuration.mem_limit) * 100);
 		let failedClass = '';
 		if(this.props.server.info.server_status.state === 'failed'){
@@ -117,18 +141,19 @@ class Servers extends Component {
 		super();
 		this.state = {
 			selectedStatus: 'all',
-			servers: null
+			servers: null,
+			searchString: ''
 		}
 		this.updateStatus = this.updateStatus.bind(this);
+		this.onSearch = this.onSearch.bind(this);
 	}
 	componentDidMount(){
 		this.updateStatus(this.state.selectedStatus);
+		this.onSearch(this.state.searchString);
 		fetchAllServers().then(function(servers){
-			console.log("fetchAllServers", servers)
 			this.setState(function(){
 				return {servers: servers}
-			})
-			
+			})		
 		}.bind(this));
 	}
 	updateStatus(status){
@@ -136,19 +161,37 @@ class Servers extends Component {
 			return{
 				selectedStatus: status
 			}
+		})
+	}
+	onSearch(searchString){
+		this.setState(function(){
+			return{
+				searchInput: searchString
+			}
 		});
-
-		
 	}
 	render() {
 		return(
 			
 				<div>
-				{!this.state.servers ? <p>loading</p> : <div><SelectedStatus
-					selectedStatus = {this.state.selectedStatus}
-					onSelect = {this.updateStatus}
-					servers={this.state.servers} />
-					<ServersTable servers={this.state.servers} status={this.state.selectedStatus} /></div>}
+				{!this.state.servers ? <p>loading</p> : 
+					<div>
+						<SelectedStatus
+							selectedStatus = {this.state.selectedStatus}
+							onSelect = {this.updateStatus}
+							servers={this.state.servers} />
+						<Search 
+							servers={this.state.servers}
+							onSearch={this.onSearch}
+							value={this.state.searchString}
+						/>
+						<ServersTable 
+							search={this.state.searchInput}
+							servers={this.state.servers} 
+							status={this.state.selectedStatus} 
+						/>
+					</div>
+				}
 				
 			</div>
 		)
